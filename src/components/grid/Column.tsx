d63f8bacde66da9783eda12ad4dce4e01c9b951b
Matthew Tramantano <matthew.tramantano@dish.com>
2021-11-03 13:32:43 -0600

    fixed grid columns

--- src/components/grid/Column.tsx
+++ src/components/grid/Column.tsx
@@ -1,28 +1,78 @@
 import React from "react";
 import styled from "styled-components";
-import { mediaDevices, MediaDevices } from "./types";
+import { MediaDevices, ColSpan } from "./types";
 
 interface ColumnProps {
     device?: MediaDevices;
     colSpan?: number | 'auto';
 }
 
-const getWidth = (colSpan: number | 'auto'): number => {
-    if(colSpan === 'auto') return 100; // auto
-    if(colSpan < 1 || colSpan > 12) return 100; // Range
-    if(colSpan % 3 === 0) return (colSpan / 3) * 25; // Modulus 3 rounder
-    return 8.333 * colSpan;
+interface BaseColumnProps {
+    colSpan: ColSpan;
 }
 
-export const Column = styled.div.attrs((props: ColumnProps) => ({
-    className: `col-${props.device ? (props.device === 'auto' ? 'md' : props.device ) : 'md'}`,
-    width: props.colSpan ? `${getWidth(props.colSpan)}%` : `${getWidth('auto')}%`,
-    media: props.device ? mediaDevices[props.device] : mediaDevices['auto'],
-}))`
-    @media ${props => props.media} {
-       width: ${props => props.width};
-    }
+// Styled Components
+const BaseColumn = styled.div`
     float: left;
     padding: 15px;
     width: 100%;
 `;
+const xsmColumn = styled(BaseColumn).attrs({
+    className: 'col-xsm'
+})<BaseColumnProps>`
+    @media only screen and (min-width: 600px) {
+        width: ${({ colSpan }) => `${getWidth(colSpan)}%`}
+    }
+`;
+const smColumn = styled(BaseColumn).attrs({
+    className: 'col-sm'
+})<BaseColumnProps>`
+    @media only screen and (max-width: 600px) {
+        width: ${({ colSpan }) => `${getWidth(colSpan)}%`}
+    }
+`;
+const mdColumn = styled(BaseColumn).attrs({
+    className: 'col-md'
+})<BaseColumnProps>`
+    @media only screen and (max-width: 600px) {
+        width: ${({ colSpan }) => `${getWidth(colSpan)}%`}
+    }
+`;
+const lgColumn = styled(BaseColumn).attrs({
+    className: 'col-lg'
+})<BaseColumnProps>`
+    @media only screen and (max-width: 600px) {
+        width: ${({ colSpan }) => `${getWidth(colSpan)}%`}
+    }
+`;
+const xlgColumn = styled(BaseColumn).attrs({
+    className: 'col-xlg'
+})<BaseColumnProps>`
+    @media only screen and (max-width: 600px) {
+        width: ${({ colSpan }) => `${getWidth(colSpan)}%`}
+    }
+`;
+
+const getWidth = (colSpan: ColSpan): number => {
+    if (colSpan === 'auto') return 100; // auto = 100&
+    if (colSpan % 3 === 0) return (colSpan / 3) * 25; // Modulus 3 rounder
+    return 8.333 * colSpan;
+}
+
+export const Column = (props: ColumnProps) => {
+    switch (props.device) {
+        case 'xsm':
+            return xsmColumn;
+        case 'sm':
+            return smColumn;
+        case 'md':
+            return mdColumn;
+        case 'lg':
+            return lgColumn;
+        case 'xlg':
+            return xlgColumn;
+        case 'auto':
+        default:
+            return mdColumn;
+    }
+}
